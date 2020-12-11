4c1a4b4b4d79a4f2dc2b44c98b28041284dbf6d8
ShadowOracle19 <lucas.g@hotmail.ca>
2020-12-11 15:56:47 -0500

    first commit

---
+++ Downloads/GAME2014-F2020-Lab11b-main/GAME2014-F2020-Lab11b-main/Assets/_Scripts/GoFastTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoFastTile : MonoBehaviour
+{
+    public PlayerBehaviour playerBehaviour;
+    public float verticalForce = 1500;
+    //public float LaunchHeight = 2000;
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerBehaviour.m_rigidBody2D.AddForce(Vector2.right * verticalForce);
+        //playerBehaviour.m_rigidBody2D.AddForce(Vector2.up * LaunchHeight);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //playerBehaviour.horizontalForce = 1500;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //playerBehaviour.m_rigidBody2D.AddForce(Vector2.right * verticalForce);
+    }
+}
---
+++ Downloads/GAME2014-F2020-Lab11b-main/GAME2014-F2020-Lab11b-main/Assets/_Scripts/JumpTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTile : MonoBehaviour
+{
+    public PlayerBehaviour playerBehaviour;
+    public float verticalForce = 1500;
+    public float LaunchHeight = 2000;
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerBehaviour.m_rigidBody2D.AddForce(Vector2.right * verticalForce);
+        playerBehaviour.m_rigidBody2D.AddForce(Vector2.up * LaunchHeight);
+    }
+    
+}
